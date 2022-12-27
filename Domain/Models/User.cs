48b397a682278df789772c08408592acd17e0e54
Dj-Jes <95280143+Dj-Jes@users.noreply.github.com>
2022-12-27 16:55:09 +0100

    all in one

---
+++ Domain/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace Domain.Models;
+
+public class User
+{
+    [Key]
+    public int Id { get; set; }
+    [MaxLength(20)]
+    public string UserName { get; set; }
+    [JsonIgnore]
+    public ICollection<Todo> Todos { get; set; }
+}
